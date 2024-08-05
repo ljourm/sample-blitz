c8215be7c0762834a7c64657ad60c086e5e6b303
ljourm <ljourm@gmail.com>
2024-08-06 00:18:01 +0900

    feat: Chakra UIの導入

---
+++ src/app/chakra/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Link } from "@chakra-ui/next-js"
+import { Button } from "@chakra-ui/react"
+
+export default function Page() {
+  return (
+    <>
+      <Link href="/about" color="blue.400" _hover={{ color: "blue.500" }}>
+        About
+      </Link>
+      <Button>Button</Button>
+    </>
+  )
+}
